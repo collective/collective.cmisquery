2ebb3606ee250e29ecfd2b8ad75c9b301595f8d7
Sylvain Viollon <thefunny@gmail.com>
2012-12-03 15:06:19 +0100

    Initial sekelton.

---
+++ README.txt
@@ -0,0 +1,7 @@
+====================
+collective.cmisquery
+====================
+
+This package is an extension to `collective.cmisbrowser`. It will let
+you enter a custom CMIS query, do the search in the browser and
+display the results as a public view.
---
+++ setup.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2012 Vlaamse Overheid. All rights reserved.
+# See also LICENSE.txt
+
+import os, sys
+from setuptools import setup, find_packages
+
+version = '1.0dev'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('docs/HISTORY.txt')
+    )
+requires = [
+    "setuptools",
+    "collective.cmisbrowser"
+    ]
+
+tests_requires = [
+    "Products.PloneTestCase"
+    ]
+
+setup(name='collective.cmisquery',
+      version=version,
+      description="CMIS repository query for Plone",
+      long_description=long_description,
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        ],
+      keywords='CMIS connection query browser plone',
+      author='Sylvain Viollon',
+      author_email='sylvain@infrae.com',
+      url='http://pypi.python.org/pypi/collective.cmisquery',
+      license='GPL',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require = tests_requires,
+      extras_require = {'test': tests_requires},
+      )
